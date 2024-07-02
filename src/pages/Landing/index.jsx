4a7e7101acb5cdeed860fa751ada67ea1649e15d
elikandid <edogbey64@gmail.com>
2024-07-02 11:22:06 +0000

    Hero page changes

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,7 +1,16 @@
+import About from "./components/about"
+import EventsCalendar from "./components/eventsCalendar"
+import Hero from "./components/hero"
+import UpcomingEvents from "./components/upcomingEvents"
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <div>
+      <Hero/>
+      <About/>
+      <UpcomingEvents/>
+      <EventsCalendar/>
+    </div>
   )
 }
 
